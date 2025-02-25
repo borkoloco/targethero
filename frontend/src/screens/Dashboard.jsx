2ed5f923a1be2eea4560229549c2d21911ab4e31
borkoloco <santiagobosca@gmail.com>
2025-02-24 22:04:30 -0300

    red

--- frontend/src/screens/Dashboard.jsx
+++ frontend/src/screens/Dashboard.jsx
@@ -1,7 +1,7 @@
 function Dashboard() {
   return (
     <div className="p-4">
-      <h1 className="text-2xl font-bold">Dashboard</h1>
+      <h1 className="text-5xl font-bold text-red-500">Dashboard</h1>
     </div>
   );
 }
